89566701bdc18079ab0b322a5c5c600bdc430bd2
Andrew Mukare <dremukare@gmail.com>
2021-12-14 18:27:24 +0000

    checking for file existence

--- markdown2html.py
+++ markdown2html.py
@@ -4,12 +4,11 @@ import sys
 from os import path
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
+    if len(sys.argv) != 3:
+        print('Usage: ./markdown2html.py README.md README.html')
         exit(1)
     if not path.isfile(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        print('Missing {}'.format(sys.argv[1]))
         exit(1)
     else:
         print()
