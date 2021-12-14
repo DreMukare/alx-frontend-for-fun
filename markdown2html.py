3aa3384354f07b162a3378c9c8be0d7293dc2225
Andrew Mukare <dremukare@gmail.com>
2021-12-14 18:11:26 +0000

    exiting 1

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,9 @@ from os import path
 
 if len(sys.argv) < 2:
     print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+    exit(1)
 elif not path.exists(sys.argv[1]):
     print('Missing README.md', file=sys.stderr)
+    exit(1)
 else:
     exit(0)
