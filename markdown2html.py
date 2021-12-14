477e39b06009418ecf0179c5f70c981ea3f4bc1c
Andrew Mukare <dremukare@gmail.com>
2021-12-14 18:50:51 +0000

    checking for file existence

--- markdown2html.py
+++ markdown2html.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 ''' parses markdown to html '''
-
+import os
+import sys
 
 if __name__ == '__main__':
-    import os
-    import sys
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print('Usage: ./markdown2html.py README.md README.html')
         exit(1)
     if os.path.isfile(sys.argv[1]) == False:
