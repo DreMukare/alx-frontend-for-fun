2cce1561d3ae9f612a3fb5e7decd67f34faefe28
Andrew Mukare <dremukare@gmail.com>
2021-12-14 18:03:13 +0000

    checking for file existence

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ from os import path
 
 if len(sys.argv) < 2:
     print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-elif not os.path.exists(sys.argv[1]):
+elif not path.exists(sys.argv[1]):
     print('Missing README.md', file=sys.stderr)
 else:
     exit(0)
